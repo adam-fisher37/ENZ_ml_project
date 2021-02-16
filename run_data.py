4ef9444ec6be2fd78189ac79ecc38ab143aa4c22
adam-fisher37 <adf49@case.edu>
2021-02-16 17:14:10 -0500

    made a model! (yay), next update tests model, NOTE: include wavelength range in comments files from now on, generated some data to test the model

--- run_data.py
+++ run_data.py
@@ -7,7 +7,7 @@ import numpy as np
 
 # variables
 ang = np.array([55.,60,65])
-com = 'angles: 55-5-65deg'
+com = 'angles: 55-5-65deg, dataset = data, wvl = [400,1000]nm'
 setlen = 500
 
 # run the boi
